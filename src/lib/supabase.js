cd8d872393cf42d480bd0344d8164236f43a0410
Nunthinee Kumjun <nunthineek68@nu.ac.th>
2026-07-20 00:11:53 +0700

    แก้การเชื่อม Supabase บน Vercel

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,12 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabaseUrl = "https://hcotkzxuxdvlofhpgjrw.supabase.co";
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error(
-    "ไม่พบค่า Supabase กรุณาตรวจสอบไฟล์ .env.local"
-  );
-}
+const supabaseKey =
+  "sb_publishable_BUrYQ50R6r-DdhdrjOMrkA_gkCDRRU8";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
